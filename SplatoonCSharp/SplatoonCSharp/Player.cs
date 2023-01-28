d3d4deed90624249baf7f806201a52f9903fd500
Jeremy Callinan <jeremy.callinan@gmail.com>
2023-01-28 03:00:58 -0500

    First version

---
+++ SplatoonCSharp/SplatoonCSharp/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonCSharp
+{
+    public enum InkColor
+    {
+        Pink,
+        Orange,
+        Green,
+        Blue,
+        Yellow
+    }
+    public class Player
+    {
+        //attributes
+        public string Name { get; set; }
+        public InkColor Color { get; set; } = InkColor.Pink;
+
+        public void setInkColor()
+        {
+            //set InkColor to a random color
+            Array colors = Enum.GetValues(typeof(InkColor));
+            Random rand = new Random();
+            this.Color = (InkColor)colors.GetValue(rand.Next(colors.Length));
+        }
+    }
+}
---
+++ SplatoonCSharp/SplatoonCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SplatoonCSharp;
+
+Console.WriteLine("Welcome to Splatoon 4!");
+Console.WriteLine("Enter Player 1's name:");
+Player player1 = new SplatoonCSharp.Player();
+string player1Name = Console.ReadLine();
+player1.Name = player1Name;
+player1.setInkColor();
+
+Console.WriteLine("Enter Player 2's name:");
+Player player2 = new SplatoonCSharp.Player();
+string player2Name = Console.ReadLine();
+player2.Name = player2Name;
+
+while (player2.Color == player1.Color)
+{
+    player2.setInkColor();  
+}
+List<Player> playerList = new List<SplatoonCSharp.Player>();
+playerList.Add(player1);    
+playerList.Add(player2);
+
+foreach (Player p in playerList)
+{
+    Console.WriteLine(p.Name + " is using ink color:" + p.Color);
+}
